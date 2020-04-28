45e741a191fe76b2301fcdf4df91feb4b4091b53
v4rp1ng <remosteiner@msn.com>
2020-04-28 20:15:30 +0200

    edited app.py

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
-print('hello world')
+while True:
+    try:
+        zahl1 = int(input('Bitte erste Zahl eingeben: '))
+        operator = input('Bitte Operator eingeben: ')
+        zahl2 = int(input('Bitte zweite Zahl eingeben: '))
+        break
+    except:
+        print('Es dürfen nur Zahlen eingegeben werden.')
+        continue
+
+if operator == '+':
+    ergebnis = zahl1 + zahl2
+elif operator == '-':
+    ergebnis = zahl1 - zahl2
+elif operator == '*':
+    ergebnis = zahl1 * zahl2
+elif operator == '/':
+    if zahl2 == 0:
+        print('Division durch Null nicht möglich.')
+        ergebnis = 'error'
+    else:
+        ergebnis = zahl1 / zahl2
+
+print('Das Ergebnis ist:', ergebnis)
+
+
